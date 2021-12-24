562cf4cfb58de062cfee83a9c220cb22a20d4339
Pritesh Jain <pritesh.jain@sifars.com>
2021-12-24 22:28:53 +0530

    WebSocket Poc

--- websocket/settings.py
+++ websocket/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'integers',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'websocket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'websocket.wsgi.application'
+ASGI_APPLICATION = 'websocket.asgi.application'
 
 
 # Database
